859ae22b5ce248c3ed28b80805ef47e2dd601bd6
nemanja-jevtovic <jevta993@gmail.com>
2018-01-14 23:01:00 +0100

    reorganize code for fetching candidates

--- js/candidates.js
+++ js/candidates.js
@@ -1,47 +1,66 @@
 (function () {
-    var request = new XMLHttpRequest();
-    var url = BASE_URL + "candidates";
+
     var listContainer = document.querySelector(".listContainer");
 
+    function displayCandidate(candidate) {
+        var div = document.createElement("div");
+        var card = document.createElement("div");
+        var image = document.createElement("img");
+        var name = document.createElement("h4");
+        var email = document.createElement("small");
+        var defaultAvatar = "assets/images/avatar.png";
+
+        div.classList.add("offset-1", "col-10", "offset-md-0", "col-md-6", "offset-lg-0", "col-lg-4", "offset-xl-0", "col-xl-3");
+        card.classList.add("card", "w-100", "p-3", "mx-auto", "my-2");
+
+        image.src = candidate.avatar
+            ? candidate.avatar
+            : defaultAvatar;
+        image.alt = "Candidate picture";
+        image.classList.add("card-img-top", "rounded-circle", "w-75", "mx-auto");
+
+        name.textContent = candidate.name;
+        name.classList.add("card-title", "text-center", "my-2");
+
+        email.textContent = candidate.email;
+        email.classList.add("card-text", "text-center");
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(email);
+        div.appendChild(card);
+        listContainer.appendChild(div);
+    }
+
+    function displayErrorMessage(cause) {
+        var errorMessage = cause === "connection"
+            ? "Looks like the server is not responding. Don't worry, we're looking into it!"
+            : "Looks like there was some kind of error. Don't worry, we're looking into it!";
+
+        var errorElement = document.createElement("h5");
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add("mx-auto", "text-justify", "mt-4", "p-3");
+        listContainer.appendChild(errorElement);
+    }
+
+    var request = new XMLHttpRequest();
+
+    var url = BASE_URL + "candidates";
     request.open("GET", url);
 
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var candidates = JSON.parse(request.responseText);
             candidates.map(function (candidate) {
-                var div = document.createElement("div");
-                var card = document.createElement("div");
-                var image = document.createElement("img");
-                var name = document.createElement("h4");
-                var email = document.createElement("small");
-                var defaultAvatar = "assets/images/avatar.png";
-
-                div.classList.add("offset-1", "col-10", "offset-md-0", "col-md-6", "offset-lg-0", "col-lg-4", "offset-xl-0", "col-xl-3");
-                card.classList.add("card", "w-100", "p-3", "mx-auto", "my-2");
-
-                image.classList.add("card-img-top", "rounded-circle", "w-75", "mx-auto");
-                image.src = candidate.avatar ? candidate.avatar : defaultAvatar;
-                image.alt = "Candidate picture";
-
-                name.textContent = candidate.name;
-                name.classList.add("card-title", "text-center", "my-2");
-
-                email.textContent = candidate.email;
-                email.classList.add("card-text", "text-center");
-
-                card.appendChild(image);
-                card.appendChild(name);
-                card.appendChild(email);
-                div.appendChild(card);
-                listContainer.appendChild(div);
+                displayCandidate(candidate);
             })
         } else {
-            listContainer.innerHTML = "<h5 class=\"mx-auto text-justify mt-4 p-3\">Looks like there was some kind of error. Don't worry, we're looking into it!</h5>";
+            displayErrorMessage();
         }
     };
 
     request.onerror = function () {
-        listContainer.innerHTML = "<h5 class=\"mx-auto text-justify mt-4 p-3\">Looks like the server is not responding. Don't worry, we're looking into it!</h5>";
+        displayErrorMessage("connection");
     };
 
     request.send();
