438dda3c7a223b71fed5b3201eb519a904b9bb76
nemanja-jevtovic <jevta993@gmail.com>
2018-01-14 18:36:40 +0100

    implement fetch candidates list from API

--- js/candidates.js
+++ js/candidates.js
@@ -2,14 +2,34 @@
     var request = new XMLHttpRequest();
     var url = BASE_URL + "candidates";
     
-    var main = document.querySelector("main");
+    function appendChildren () {
+        var element = arguments[0];
+        for (var i = 1; i < arguments.length; i++) {
+            var child = arguments[i];
+            element.appendChild(child);
+        }
+    }
     
     request.open("GET", url);
     
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            console.log(data);
+            var candidates = JSON.parse(request.responseText);
+            candidates.map(function (candidate) {
+                var main = document.querySelector("main");
+                var div = document.createElement("div");
+                var image = document.createElement("img");
+                var name = document.createElement("h4");
+                var email = document.createElement("p");
+
+                image.src = candidate.avatar;
+                image.alt = "Candidate picture";
+                name.textContent = candidate.name;
+                email.textContent = candidate.email;
+
+                appendChildren(div, image, name, email);
+                main.appendChild(div);  
+            })
         } else {
             main.innerHTML = "<p>Looks like there was some kind of error. Don't worry, we're looking into it!</p>";
         }
